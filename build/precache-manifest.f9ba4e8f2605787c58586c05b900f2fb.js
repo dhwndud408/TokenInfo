211464d28d11e009730ae2029eb63d948448a715
dhwndud407 <dhwndud407@gmail.com>
2020-07-27 22:56:54 +0900

    no message

---
+++ build/precache-manifest.f9ba4e8f2605787c58586c05b900f2fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0ddccc500b040d5b04e3773de342a0",
+    "url": "/TokenInfo/index.html"
+  },
+  {
+    "revision": "1fb3e67c8c389df13214",
+    "url": "/TokenInfo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d1be7743aafef7d8ff0",
+    "url": "/TokenInfo/static/js/2.04b63b3e.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/TokenInfo/static/js/2.04b63b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb3e67c8c389df13214",
+    "url": "/TokenInfo/static/js/main.7a59e87d.chunk.js"
+  },
+  {
+    "revision": "dc2218fc7a8bfcd4d51a",
+    "url": "/TokenInfo/static/js/runtime-main.6f0fbd1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TokenInfo/static/media/logo.5d5d9eef.svg"
+  }
+]);
